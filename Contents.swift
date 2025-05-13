2de41b98d0c5c4090be6cad1c3bbb0e381b215a0
Edwin Cardenas <ecardenas@lwolf.com>
2025-05-13 13:24:09 -0500

    feat: implement comparable protocol

--- Contents.swift
+++ Contents.swift
@@ -10,9 +10,13 @@ struct Point {
 //    return (lhs.x == rhs.x) && (lhs.y == rhs.y)
 //}
 
-extension Point: Equatable {
+extension Point: Equatable, Comparable {
     static func ==(lhs: Point, rhs: Point) -> Bool {
         return (lhs.x == rhs.x) && (lhs.y == rhs.y)
+    }
+    
+    static func <(lhs: Point, rhs: Point) -> Bool {
+        return (lhs.x < rhs.x) && (lhs.y < rhs.y)
     }
 }
 
@@ -23,3 +27,11 @@ let abNotEqual = (a != b)
 
 print(abEqual) // true
 print(abNotEqual) // false
+
+let c = Point(x: 2, y: 6)
+let d = Point(x: 3, y: 7)
+let cdEqual = (c == d)
+let cLessThanD = (c < d)
+
+print(cdEqual) // false
+print(cLessThanD) // true
